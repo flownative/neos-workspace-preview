ecc97239d59fdeba65031284f2b376dc209ec6c6
bwaidelich <b.waidelich@wwwision.de>
2019-09-16 15:15:30 +0200

    FEATURE: Some tweaks and improvements
    * i18n for the Inspector integration
    * Adapt to Neos Styling (reusing Neos.Ui components)
    * Render reason when hiding "Copy Preview Link" button

--- Resources/Private/JavaScript/LinkView/src/LinkView.js
+++ Resources/Private/JavaScript/LinkView/src/LinkView.js
@@ -3,8 +3,13 @@ import PropTypes from 'prop-types';
 import Clipboard from 'react-clipboard.js';
 import {dataLoader} from '@neos-project/neos-ui-views';
 import {$get} from "plow-js";
+import {neos} from '@neos-project/neos-ui-decorators';
+import {Icon, Button} from '@neos-project/react-ui-components';
 
 @dataLoader()
+@neos(globalRegistry => ({
+  i18nRegistry: globalRegistry.get('i18n'),
+}))
 export default class LinkView extends PureComponent {
   static propTypes = {
     data: PropTypes.object.isRequired
@@ -12,16 +17,23 @@ export default class LinkView extends PureComponent {
 
   render() {
     const linkUrl = $get(['link'], this.props.data);
+    const error = $get(['error'], this.props.data);
 
-    if (linkUrl === '---') {
+    if (error !== 'none') {
       return (
-        <div>---</div>
+        <div>{this.props.i18nRegistry.translate('Flownative.WorkspacePreview:Main:error.' + error, 'Error: ' + error)}</div>
       )
     }
-
     return (
       <div>
-        <Clipboard data-clipboard-text={linkUrl}>Copy Preview Link</Clipboard>
+        <Clipboard
+            data-clipboard-text={linkUrl}
+            component={Button}
+            button-style="lighter"
+        >
+          <Icon icon="copy" padded="right" />
+          {this.props.i18nRegistry.translate('Flownative.WorkspacePreview:Main:copyPreviewLink', 'Copy Preview Link')}
+        </Clipboard>
       </div>
     );
   }
